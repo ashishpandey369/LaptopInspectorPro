1aa8112e22d9dc27cdc37a7adc557f579278956a
Ashish Pandey <ap965599@gmail.com>
2026-08-20 09:49:08 +0530

    Implement peripheral port diagnostics

--- modules/Ports.ps1
+++ modules/Ports.ps1
@@ -1,1 +1,3 @@
-# TODO: USB/Ports tests
+function Get-LIPPortsInfo {
+    try { @((Get-PnpDevice -PresentOnly -ErrorAction Stop) | Where-Object {$_.Class -in @('USB','Bluetooth','Ports')} | Select-Object -First 100 | ForEach-Object { [pscustomobject]@{Status=$_.Status; Class=$_.Class; Name=$_.FriendlyName; InstanceId=$_.InstanceId} }) } catch { [pscustomobject]@{Status='Unavailable';Error=$_.Exception.Message} }
+}
