b390e59503a4ddb03b3bd1803a50898884393d3a
Ashish Pandey <ap965599@gmail.com>
2026-08-20 09:49:12 +0530

    Implement touch diagnostics

--- modules/Touch.ps1
+++ modules/Touch.ps1
@@ -1,1 +1,3 @@
-# TODO: Touch tests
+function Get-LIPTouchInfo {
+    try { $d=@(Get-PnpDevice -PresentOnly -ErrorAction Stop | Where-Object {$_.Class -match 'HID' -or $_.FriendlyName -match 'Touch|Digitizer'}); [pscustomobject]@{Status='OK';Detected=($d.Count -gt 0);Devices=$d.Count;Names=($d.FriendlyName -join '; ')} } catch { [pscustomobject]@{Status='Unavailable';Error=$_.Exception.Message} }
+}
